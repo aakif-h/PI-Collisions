edaf8b67ef0f1fcf053ea0c613560d397a3c17ed
aakif-h <aakifhussaini@gmail.com>
2019-09-06 15:52:02 -0700

    Fifth commit
    Resolved background color issue. Added in a second block and fixed block
    to block collision detection. Added a counter to show the number of
    collisions made during the simulation. Current issue is the collision
    count does not reflect the digits of pi.

--- src/collision/CollisionMain.java
+++ src/collision/CollisionMain.java
@@ -3,43 +3,63 @@ import collision.Block;
 import processing.core.*;
 
 public class CollisionMain extends PApplet {
-	int winX = 800;
-	int winY = 500;
-	int groundLevel = winY - 100;
+	//global variables
+	int winX;
+	int winY;
+	int collisionCounter;
+	int groundLevel;
 	Block block1, block2;
 	
 	public static void main(String[] args) {
 		PApplet.main("collision.CollisionMain");
 	}
 	
+	// initializing canvas
 	public void settings() {
+		winX = 800;
+		winY = 500;
+		
 		size(winX, winY);
 	}
 	
+	// defining application conditions
 	public void setup() {
-		block1 = new Block();
-		block2 = new Block();
+		collisionCounter = 0;
+		groundLevel = winY - 100;
+		
+		block1 = new Block(100, 0, 10);
+		block2 = new Block(500, -2, 1000);
 	}
 	
 	public void draw() {
-		clear();
 		drawBackground();
 		drawBlock(block1);
-		//block2.drawBlock();
+		drawBlock(block2);
 		
-		//checkBlockCollision(block1, block2);
+		checkBlockCollision(block1, block2);
 		
 		checkWallCollision(block1);
-		//checkWallCollision(block2);
-		
+		checkWallCollision(block2);		
 	}
 	
 	public void drawBackground() {
+		//gray background
+		fill(150);
+		rect(-1, -1, winX+1, winY+1);
+		
 		//dark blue floor
 		fill(0, 25, 51);
 		rect(-1, groundLevel, winX+2, winY - groundLevel);
+		
+		
+		fill(255);
+		textAlign(CENTER);
+		textSize(18);
+		text("Num Collisions: ", winX/2, winY - 50);
+		text(collisionCounter, winX/2 + 85, winY - 50);
 	}
 	
+	//to not only draw the Block, but also update the position of the block
 	public void drawBlock(Block b) {
 		
 		fill(76, 153, 0);
@@ -49,11 +69,14 @@ public class CollisionMain extends PApplet {
 	
 	// simulate a perfectly elastic collision (no energy is lost to heat, noise, etc)
 	public void checkBlockCollision(Block b1, Block b2) {
-		if (b1.getPosition() <= b2.getPosition() - b1.getWidth() ||
-			b2.getPosition() <= b1.getPosition() - b2.getWidth() ) {
+		if ( (b1.getPosition() >= b2.getPosition() && b1.getPosition() <= b2.getPosition() + b2.getWidth() ) ||
+			 (b1.getPosition() + b1.getWidth() >= b2.getPosition() && b1.getPosition() + b1.getWidth() <= b2.getPosition() + b2.getWidth() ) 
+		   ) 
+		{
 			int[] temp = perfectlyElasticCollision(b1.getVelocity(), b2.getVelocity(), b1.getWidth(), b2.getWidth());
 			b1.setVelocity(temp[0]);
 			b2.setVelocity(temp[1]);
+			collisionCounter++;
 		}
 	}
 	
@@ -63,8 +86,9 @@ public class CollisionMain extends PApplet {
 	
 	// perfectly elastic collision against object of infinite mass (wall)
 	public void checkWallCollision(Block b) {
-		if (b.getPosition() <= 0 || b.getPosition() + b.getWidth() >= winX) {
+		if (b.getPosition() <= 0 && b.getVelocity() < 0) {
 			b.setVelocity(-b.getVelocity());
+			collisionCounter++;
 		}
 	}
 	
